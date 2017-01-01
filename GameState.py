91ccdfe5a09659f1d520464c097d104b2ecf6017
Will Rollins <willrollins@Wills-MacBook-Pro.local>
2016-12-31 22:38:45 -0700

    bunch of rough changes, readMe created, ToDo list created

--- GameState.py
+++ GameState.py
@@ -1,12 +1,33 @@
 class GameState:
-    __init__(self):
+    def __init__(self):
         # replace all of the pieces with actual chessPieces
-        self.board =
-        [["R", "N", "B", "K", "Q", "B", "N", "K"],
-        ["P", "P", "P", "P", "P", "P", "P", "P"],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        ["P", "P", "P", "P", "P", "P", "P", "P"],
-        ["R", "N", "B", "K", "Q", "B", "N", "K"]]
+        # is it quicker to keep it like this, or put chessPiece objects into the board
+        # this allows the list to be smaller in memory, but requires a check to
+        # see what type of piece it is
+        # should I represent each piece as a number? even smaller in memory
+        # how to represent ownership of a piece?
+
+        # thoughts:
+        # assign the value of a piece to its name ie: a pawn = a 1
+        # this would simplify the heuristics as to access the value of a piece
+        # you would just have to compare the names in a piece swap to decide
+        # who would win that exchange
+        self.board = [[04, 03, 02, 01000, 08, 02, 03, 04], [01, 01, 01, 01, 01, 01, 01, 01], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], ["P", "P", "P", "P", "P", "P", "P", "P"], ["R", "N", "B", "K", "Q", "B", "N", "K"]]
+    # A move is a list of two tuples, the piece to be moved, and its location
+    # to move to
+    # first tuple is the current location of the piece to be moved
+    # the legality of the move has already been checked when building the tree
+    # this allows you to remove illegal moves (ie: going on top of your own
+    # piece), offboard moves are taken care of in chessPiece.py
+    # branches with illigal moves are trimmed and aren't searched
+    def makeMove(self, board, move):
+        # declare lists of the move spaces
+        piece = [move[0][0], move[0][1]]
+        moveTo = [move[1][0], move[1][1]]
+        # make the move
+        board[moveTo[0]][moveTo[1]] = board[piece[0]][piece[1]]
+        board[piece[0]][piece[1]] = 0
+        return board
+
+
+    def checkMoveLegality(self, board, move):
---
+++ readMe.txt
@@ -0,0 +1,22 @@
+Welcome to the read me, here I will explain the value of the chess pieces, and
+other stuff that may come up in the code.
+
+piecesCode:
+
+To code for each piece contains to integers, XY
+X:
+  -declares the ownership of the piece
+  0 means that it is owned by the Glasses wearer
+  1 means that it is owned by the opponent
+y:
+  -declares which piece it is
+  -also is the value of the piece based on chess people who have knowledge of
+  chess
+  //EDIT THIS TO BE ACCURATE
+  //also, we can play with the values in order to get the most effective AI
+  1: Pawn
+  2: Bishop
+  3: Knight
+  4: Rook
+  8: Queen
+  1000: King
